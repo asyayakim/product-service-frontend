1e9ed3c3f7ee1935b7b59e415cfd5145ccccadcc
asyayakim <asyayakimova@yahoo.com>
2025-10-08 15:15:30 +0200

    Update Loading.tsx

--- product-service-app/src/components/elements/Loading.tsx
+++ product-service-app/src/components/elements/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <section id="hero" className="py-12 hero section bg-gray-50">
-            <div className="max-w-md p-6 mx-auto text-center bg-white border hero-container rounded-xl">
+            <div className="flex items-center justify-center p-6 mx-auto text-center bg-white border ax-w-md hero-container rounded-xl">
                 <div className="flex flex-col items-center justify-center space-y-4">
                     {/* <div className="w-10 h-10 border-4 border-gray-300 rounded-full border-t-blue-500 animate-spin"></div> */}
                     {/* <p className="text-lg font-medium text-gray-700">Loading products...</p> */}
